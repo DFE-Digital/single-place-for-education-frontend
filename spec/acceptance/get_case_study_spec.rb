36c6741b5050a2fe1b9f8852946e52d991afcada
Wen Ting Wang <chubberlisk@outlook.com>
2019-06-10 14:10:42 +0100

    Skip acceptance test as gateway not implemented yet
    Co-authored-by: Robert McHugh <robert@madetech.com>

--- spec/acceptance/get_case_study_spec.rb
+++ spec/acceptance/get_case_study_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Get Case Study' do
-  it 'can retrieve the content of a case study page' do
+  xit 'can retrieve the content of a case study page' do
     case_study_slug = 'case-study-grantham-primary-school'
     contentful_gateway = Gateway::ContentfulGateway.new(
       space_id: '',
@@ -11,6 +11,18 @@ describe 'Get Case Study' do
 
     response = get_case_study.execute(slug: case_study_slug)
 
-    expect(response).to eq({ name: 'Case Study - Grantham Primary School' })
+    expect(response).to eq({
+      name: 'Case Study - Grantham Primary School',
+      slug: 'case-study-grantham-primary-school',
+      hero_image: '//images.ctfassets.net/grantham-hero-image.png',
+      content: [{
+        type: :heading,
+        data: {
+          text: 'School leader transforms Grantham primary school',
+          level: :heading_one,
+          bold: true
+        }
+      }]
+    })
   end
 end
