5331ba8e468d701020e324bf0353efb9bef96ad9
Wen Ting Wang <chubberlisk@outlook.com>
2019-06-10 10:13:14 +0100

    Add rails_helper

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -1,3 +1,4 @@
+require 'rails_helper'
 describe 'example' do
   it 'adds two numbers' do
     expect(2 + 2).to eq(4)
---
+++ spec/rails_helper.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+require 'rails/test_help'
