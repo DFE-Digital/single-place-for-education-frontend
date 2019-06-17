8d7a6f542eee27068bbeb1b0203e034ef3c185b5
Wen Ting Wang <chubberlisk@outlook.com>
2019-06-17 17:07:28 +0100

    Create sub-category page

---
+++ app/controllers/sub_category_controller.rb
@@ -0,0 +1,22 @@
+class SubCategoryController < ApplicationController
+  def index
+    contentful_gateway = Gateway::ContentfulGateway.new(
+      space_id: ENV['CONTENTFUL_SPACE_ID'],
+      access_token: ENV['CONTENTFUL_ACCESS_TOKEN']
+    )
+
+    temp_sub_category = contentful_gateway.get_sub_category(slug: params[:slug])
+
+    return render 'error/404', status: :not_found if temp_sub_category.nil?
+
+    @sub_category = {
+      title: temp_sub_category.title,
+      slug: temp_sub_category.slug,
+      collection_name: temp_sub_category.collection_name,
+      description: temp_sub_category.description,
+      content: temp_sub_category.content
+    }
+
+    render 'sub_category/index'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'case-study/:slug', to: 'case_study#show'
+  get 'sub-category/:slug', to: 'sub_category#index'
   get 'tvs', to: 'tvs#index'
 end
