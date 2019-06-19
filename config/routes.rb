524c8112e395a41d6daac92922e976ddcb26f060
Daniel Burnley <dan@danops.uk>
2019-06-19 16:56:23 +0100

    Add initial category pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'case-study/:slug', to: 'case_study#show'
+
+  get 'category/hiring', to: 'category#hiring'
+  get 'category/get-a-job', to: 'category#get_a_job'
+  get 'category/staff-voices', to: 'category#staff_voices'
+
   get 'category/:slug', to: 'category#index'
   get 'sub-category/:slug', to: 'sub_category#index'
   get 'tvs', to: 'tvs#index'
