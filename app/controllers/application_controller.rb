99624b5809677d57a797d8d12cdf00c7d32bc719
Wen Ting Wang <chubberlisk@outlook.com>
2019-06-27 15:01:32 +0100

    Prevent basic auth from breaking

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: ENV['BASIC_AUTH_USER'], password: ENV['BASIC_AUTH_PASSWORD']
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_USER'], password: ENV['BASIC_AUTH_PASSWORD'] if ENV['BASIC_AUTH_USER']
 end
