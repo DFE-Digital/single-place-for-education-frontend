a0859947b15971c9aa65d3f1e2efe6ac6ea9d5c1
Wen Ting Wang <chubberlisk@outlook.com>
2019-07-02 16:33:50 +0100

    Refactor order of routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,9 @@ Rails.application.routes.draw do
   get 'tvs', to: 'tvs#index'
   get 'in-development', to: 'home#placeholder'
 
-  get 'resource/how-to-run-a-retro', to: 'resource#retro'
-  get 'resource/answer-questions-using-scaffold', to: 'resource#structured_questions'
-  get 'resource/how-to-create-a-culture-of-respect-and-trust', to: 'resource#respect_and_trust'
-
   get 'guidance/:slug', to: 'guidance#show'
+
+  get 'resource/how-to-run-a-retro', to: 'resource#retro'
+  get 'resource/how-to-create-a-culture-of-respect-and-trust', to: 'resource#respect_and_trust'
+  get 'resource/answer-questions-using-scaffold', to: 'resource#structured_questions'
 end
