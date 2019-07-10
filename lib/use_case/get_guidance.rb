0abc0be244f25c9944c93ed4d2cc385fedf7106a
Robert McHugh <robert@madetech.com>
2019-07-10 09:46:22 +0100

    edit get_guidance to remove puts

--- lib/use_case/get_guidance.rb
+++ lib/use_case/get_guidance.rb
@@ -6,8 +6,6 @@ class UseCase::GetGuidance
   def execute(slug:)
     guidance = @content_gateway.get_guidance(slug: slug)
     return nil if guidance.nil?
-
-    puts guidance.last_updated
     {
       title: guidance.title,
       slug: guidance.slug,
